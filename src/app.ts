27e1f6bcbafad89060b8f39b54856d7d154c90dc
Guillaume Ducret <guillaumeducret@gmx.com>
2023-10-25 09:24:17 +0200

    Add a SplitflowDesigner instance when creating a SplitflowApp

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,127 @@
 import { Datasource, createDatasource } from './datasource'
-import { Dispatcher, createDisatcher } from './dispatch'
+import { Dispatcher, Error, createDisatcher } from './dispatch'
 import * as actions from './actions'
 import panel, { PanelState } from './stores/panel'
 import dialog, { DialogState } from './stores/dialog'
 import alert, { AlertState } from './stores/alert'
+import { SSRRegistry, SplitflowDesigner, createDesigner } from '@splitflow/designer'
 
 export interface AppConfig {
     projectId?: string
+    appId?: string
+    devtool?: boolean
+    ssr?: boolean
+    local?: boolean
+    remote?: boolean
+}
+
+export interface SplitflowApp {
+    dispatcher: Dispatcher
+    datasource: Datasource
+    designer: SplitflowDesigner
+}
+
+let defaultApp: SplitflowApp
+
+export interface SplitflowAppConstructor<T extends SplitflowApp> {
+    new (
+        dispatcher: Dispatcher,
+        datasource: Datasource,
+        designer: SplitflowDesigner,
+        config: AppConfig
+    ): T
+}
+
+export function initializeSplitflowApp(config: AppConfig): SplitflowApp
+export function initializeSplitflowApp<T extends SplitflowApp>(
+    config: AppConfig,
+    App: SplitflowAppConstructor<T>
+): T
+export function initializeSplitflowApp(
+    config: AppConfig,
+    App?: SplitflowAppConstructor<SplitflowApp>
+) {
+    App ??= SplitflowApp
+
+    if (defaultApp) {
+        defaultApp = new App(
+            defaultApp.dispatcher,
+            defaultApp.datasource,
+            defaultApp.designer,
+            config
+        )
+    } else {
+        defaultApp = createSplitflowApp(config, App)
+    }
+    return defaultApp
+}
+
+export function createSplitflowApp(config: AppConfig, registry?: SSRRegistry): SplitflowApp
+
+export function createSplitflowApp<T extends SplitflowApp>(
+    config: AppConfig,
+    App: SplitflowAppConstructor<T>
+): T
+
+export function createSplitflowApp<T extends SplitflowApp>(
+    config: AppConfig,
+    registry: SSRRegistry,
+    App: SplitflowAppConstructor<T>
+): T
+
+export function createSplitflowApp(config: AppConfig, arg2?: any, arg3?: any) {
+    const registry = isSSRRegistry(arg2) ? arg2 : undefined
+    const App = isSplitflowAppConstructor(arg2)
+        ? arg2
+        : isSplitflowAppConstructor(arg3)
+        ? arg3
+        : SplitflowApp
+
+    const dispatcher = createDisatcher()
+    const datasource = createDatasource()
+    const designer = createDesigner(config, undefined, registry)
+
+    return new App(dispatcher, datasource, designer, config)
+}
+
+export function getDefaultApp() {
+    return (defaultApp ??= createSplitflowApp({}))
 }
 
 export class SplitflowApp {
-    constructor(dispatcher: Dispatcher, datasource: Datasource, config?: AppConfig) {
+    constructor(
+        dispatcher: Dispatcher,
+        datasource: Datasource,
+        designer: SplitflowDesigner,
+        config?: AppConfig
+    ) {
         this.dispatcher = dispatcher
         this.datasource = datasource
+        this.designer = designer
         this.#config = config
+
+        dispatcher.addActionHandler('display', actions.display)
+        dispatcher.addActionHandler('discard', actions.discard)
+        dispatcher.addActionHandler('open', actions.open)
+        dispatcher.addActionHandler('cancel', actions.cancel)
+        dispatcher.addActionHandler('show', actions.show)
+
+        datasource.addResourceHandler('panel', () => panel)
+        datasource.addResourceHandler('dialog', () => dialog)
+        datasource.addResourceHandler('alert', () => alert)
     }
 
     dispatcher: Dispatcher
     datasource: Datasource
+    designer: SplitflowDesigner
     #config: AppConfig
+    #initialize: Promise<{ error?: Error }>
+
+    async initialize() {
+        return (this.#initialize ??= (async () => {
+            return this.designer.initialize()
+        })())
+    }
 
     display(panelName: string, discardPanels?: string[]) {
         const action: actions.PanelAction = { type: 'display', name: panelName, discardPanels }
@@ -35,65 +138,22 @@ export class SplitflowApp {
     }
 
     get panel() {
-        return this.datasource.fetchResource<PanelState>({name: 'panel'})
+        return this.datasource.fetchResource<PanelState>({ name: 'panel' })
     }
 
     get dialog() {
-        return this.datasource.fetchResource<DialogState>({name: 'dialog'})
+        return this.datasource.fetchResource<DialogState>({ name: 'dialog' })
     }
 
     get alert() {
-        return this.datasource.fetchResource<AlertState>({name: 'alert'})
+        return this.datasource.fetchResource<AlertState>({ name: 'alert' })
     }
 }
 
-let defaultApp: SplitflowApp
-
-export function initializeSplitflowApp(config: AppConfig): SplitflowApp
-export function initializeSplitflowApp<T extends SplitflowApp>(
-    config: AppConfig,
-    App: new (dispatcher: Dispatcher, datasource: Datasource, config: AppConfig) => T
-): T
-export function initializeSplitflowApp(
-    config: AppConfig,
-    App?: new (dispatcher: Dispatcher, datasource: Datasource, config: AppConfig) => SplitflowApp
-) {
-    App ??= SplitflowApp
-
-    if (defaultApp) {
-        defaultApp = new App(defaultApp.dispatcher, defaultApp.datasource, config)
-    } else {
-        defaultApp = createSplitflowApp(config, App)
-    }
-    return defaultApp
+function isSSRRegistry(value: any): value is SSRRegistry {
+    return value?.style !== undefined
 }
 
-export function createSplitflowApp(config: AppConfig): SplitflowApp
-export function createSplitflowApp<T extends SplitflowApp>(
-    config: AppConfig,
-    App: new (dispatcher: Dispatcher, datasource: Datasource, config: AppConfig) => T
-): T
-export function createSplitflowApp(
-    config: AppConfig,
-    App?: new (dispatcher: Dispatcher, datasource: Datasource, config: AppConfig) => SplitflowApp
-) {
-    App ??= SplitflowApp
-
-    const dispatcher = createDisatcher()
-    dispatcher.addActionHandler('display', actions.display)
-    dispatcher.addActionHandler('discard', actions.discard)
-    dispatcher.addActionHandler('open', actions.open)
-    dispatcher.addActionHandler('cancel', actions.cancel)
-    dispatcher.addActionHandler('show', actions.show)
-
-    const datasource = createDatasource()
-    datasource.addResourceHandler('panel', () => panel)
-    datasource.addResourceHandler('dialog', () => dialog)
-    datasource.addResourceHandler('alert', () => alert)
-
-    return new App(dispatcher, datasource, config)
-}
-
-export function getDefaultApp() {
-    return (defaultApp ??= createSplitflowApp({}))
+function isSplitflowAppConstructor(value: any): value is SplitflowAppConstructor<any> {
+    return typeof value === 'function'
 }
--- src/dispatch.ts
+++ src/dispatch.ts
@@ -1,3 +1,7 @@
+interface Record {
+    handler: (action: Action, context: any) => Result
+    context: any
+}
 
 export interface Action {
     type: string
@@ -6,51 +10,80 @@ export interface Action {
 export interface Result {
 }
 
+export interface ErrorResult {
+    error?: Error
+}
+
+export interface Error {
+    code: string
+    message: string
+}
+
 export interface DispatchOptions {
-    multiDispatch: boolean
+    multiDispatch?: boolean
+    discriminator?: unknown
 }
 
 export interface Dispatcher {
-    addActionHandler: (type: string, handler: (action: Action) => Result) => void;
-    removeActionHandler: (type: string, handler: (action: Action) => Result) => void;
-    dispatchAction: (action: Action, options?: DispatchOptions) => Result;
+    addActionHandler(type: string, handler: (action: Action) => Result): void
+    addActionHandler<T>(
+        type: string,
+        handler: (action: Action, context: T) => Result,
+        context: T
+    ): void
+    removeActionHandler<T>(type: string, handler: (action: Action) => Result): void
+    removeActionHandler<T>(
+        type: string,
+        handler: (action: Action, context: T) => Result,
+        context: T
+    ): void
+    dispatchAction(action: Action, options?: DispatchOptions): Result
 }
 
 export function createDisatcher(): Dispatcher {
-    const registry = new Map<string, Array<(action: Action) => Result>>()
+    const registry = new Map<string, Array<Record>>()
 
-    function addActionHandler(type: string, handler: (action: Action) => Result) {
-        let handlers = registry.get(type)
-        if (!handlers) {
-            handlers = new Array<() => boolean>()
-            registry.set(type, handlers)
+    function addActionHandler(
+        type: string,
+        handler: (action: Action, context?: any) => Result,
+        context?: any
+    ) {
+        let records = registry.get(type)
+        if (!records) {
+            records = new Array()
+            registry.set(type, records)
         }
-        if (handlers.indexOf(handler) < 0) {
-            handlers.unshift(handler)
+        if (records.findIndex((r) => r.handler === handler && r.context === context) < 0) {
+            records.unshift({ handler, context })
         }
     }
 
-    function removeActionHandler(type: string, handler: (action: Action) => Result) {
-        let handlers = registry.get(type)
-        if (handlers) {
-            const index = handlers.indexOf(handler)
-            handlers.splice(index, 1)
+    function removeActionHandler(
+        type: string,
+        handler: (action: Action, context?: any) => Result,
+        context?: any
+    ) {
+        let records = registry.get(type)
+        if (records) {
+            const index = records.findIndex((r) => r.handler === handler && r.context === context)
+            records.splice(index, 1)
         }
     }
 
     function dispatchAction(action: Action, options?: DispatchOptions): Result {
-        let handlers = registry.get(action.type)
-        if (handlers) {
-            for (const handler of handlers) {
-                const result = handler(action)
-                if (result && !options?.multiDispatch) return result
+        let records = registry.get(action.type)
+        if (records) {
+            for (const record of records) {
+                if (record.context?.accept?.(options?.discriminator) ?? true) {
+                    const result = record.handler(action, record.context)
+                    if (result && !options?.multiDispatch) return result
+                }
             }
         } else {
             console.warn(`no handler defined for action "${action.type}"`)
         }
         return {}
     }
-
 
     return {
         addActionHandler,
